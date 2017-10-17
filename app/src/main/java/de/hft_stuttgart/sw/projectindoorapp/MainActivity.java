ffb664d8b8b96faa80a01b6cb4f805ece191a9ae
Moritz Schillinger <Moritz.Schillinger@ffuf.de>
2017-10-17 17:54:38 +0200

    Basic structure of an Android app.

---
+++ app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/MainActivity.java
@@ -0,0 +1,13 @@
+package de.hft_stuttgart.sw.projectindoorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
