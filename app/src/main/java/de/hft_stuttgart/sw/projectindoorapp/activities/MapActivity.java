6d6de6c92af8461e7fde5ae296df8bb26ed090af
prathyusha <pratgermany@gmail.com>
2017-11-13 10:51:56 +0100

    test commit

--- app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/activities/MapActivity.java
+++ app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/activities/MapActivity.java
@@ -34,6 +34,7 @@ public class MapActivity extends AppCompatActivity
             }
         });
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
