621bb99ef0345b25dad2baa9de534d939c9a6f3b
Moritz Schillinger <Moritz.Schillinger@ffuf.de>
2017-11-19 14:43:54 +0100

    Add empty WifiReceiver class.

---
+++ app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/broadcast_receivers/WifiReceiver.java
@@ -0,0 +1,15 @@
+package de.hft_stuttgart.sw.projectindoorapp.broadcast_receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WifiReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
