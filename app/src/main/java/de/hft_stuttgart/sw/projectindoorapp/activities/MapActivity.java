637fd783583f26d715d23eda84c951668123daa3
vahidsheikhalizadeh <vahid.sheykhalizadeh@logicline.de>
2017-11-20 10:56:37 +0100

    first integration between google map api and the basic framework app

--- app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/activities/MapActivity.java
+++ app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/activities/MapActivity.java
@@ -1,10 +1,15 @@
 package de.hft_stuttgart.sw.projectindoorapp.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,20 +19,53 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hft_stuttgart.sw.projectindoorapp.R;
 
 public class MapActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,OnMyLocationClickListener,
+        GoogleMap.OnMyLocationButtonClickListener,OnMapReadyCallback,GoogleMap.OnGroundOverlayClickListener {
+
     private static final String TAG = "MapActivity";
+
+    private GoogleMap mMap;
+    private GroundOverlayOptions hftMap;
+    private static final LatLng HFT = new LatLng(48.779844, 9.173462);
+    private final List<BitmapDescriptor> mImages = new ArrayList<>();
+
+    // HFT building boundary
+    LatLngBounds hftBounds = new LatLngBounds(
+            new LatLng(48.779565, 9.173414),       // South west corner
+            new LatLng(48.780150, 9.173494));     //  north east corner
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -113,5 +151,80 @@ public class MapActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public void onGroundOverlayClick(GroundOverlay groundOverlay) {
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        mMap.setOnGroundOverlayClickListener(this);
+
+        // Add a marker in Stuttgart and move the camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hftBounds.getCenter(), 16));
+
+        mImages.clear();
+
+        mMap.addMarker(new MarkerOptions().position(HFT).title("HFT,Bau 2"));
+
+
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 4000, null);
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.floor_map));
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+
+        // North east corner
+
+
+
+        hftMap = new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.floor_map))
+                .bearing(65)
+                .position(HFT,56f,35f);
+
+
+        mMap.addGroundOverlay(hftMap);
+
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+
+        // TODO : show just the floor map after click
+
     }
 }
