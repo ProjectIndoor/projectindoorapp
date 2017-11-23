141593ba987c125dbb261cbb1125933a76239d2d
prathyusha <pratgermany@gmail.com>
2017-11-23 13:04:02 +0100

    Add WifiReceiver class

--- app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/broadcast_receivers/WifiReceiver.java
+++ app/src/main/java/de/hft_stuttgart/sw/projectindoorapp/broadcast_receivers/WifiReceiver.java
@@ -3,13 +3,32 @@ package de.hft_stuttgart.sw.projectindoorapp.broadcast_receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.util.List;
+
 
 public class WifiReceiver extends BroadcastReceiver {
+ WifiManager wifiManager;
+
+public WifiReceiver(WifiManager wifiManager) {
+
+
+    this.wifiManager = wifiManager;
+}
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.i("WifiReceiver","receive...");
+        List<ScanResult> scanResults = this.wifiManager.getScanResults();
+        for(int i=0;i<scanResults.size();i++){
+            ScanResult result=scanResults.get(i);
+            Log.i("WifiReceiver",result.BSSID +"......"+result.level);
+
+
+        }
     }
 }
